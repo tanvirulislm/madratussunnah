b37c5b85ceea885f0dfa749abbddf39d8f498209
Tanvirul Islam <ownbinodon@gmail.com>
2025-06-16 17:13:19 +0600

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+  document.getElementById('menuToggle').addEventListener('click', function () {
+    const menu = document.getElementById('mobileMenu');
+    menu.classList.toggle('hidden');
+    
+  });
+
+  feather.replace()
