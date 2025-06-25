fb32769ec82ca5727044b194637f8fb3a2a7edd2
Tanvirul <ownbinodon@gmail.com>
2025-06-25 20:41:15 +0600

    f

--- script.js
+++ script.js
@@ -31,3 +31,60 @@ document.getElementById('mobileAcademicToggle').addEventListener('click', functi
 });
 
   feather.replace()
+
+
+  
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('.tab-button')
+    const tabContents = document.querySelectorAll('.tab-content')
+    const viewAllButton = document.getElementById('view-all-button')
+    const scrollContainer = document.querySelector('.custom-scrollbar')
+
+    function activateTab(tabName) {
+      // Deactivate all tab buttons
+      tabs.forEach(tab => {
+        tab.classList.remove('text-teal-600', 'border-teal-600')
+        tab.classList.add('text-gray-600', 'hover:text-teal-600', 'hover:border-teal-300')
+      })
+
+      tabContents.forEach(content => {
+        content.classList.remove('active-tab')
+        content.classList.add('hidden')
+      })
+      if (scrollContainer) {
+        scrollContainer.scrollTop = 0
+      }
+
+      const activeTabButton = document.querySelector(`[data-tab="${tabName}"]`)
+      activeTabButton.classList.remove('text-gray-600', 'hover:text-teal-600', 'hover:border-teal-300')
+      activeTabButton.classList.add('text-teal-600', 'border-teal-600')
+
+      const activeTabContent = document.getElementById(`tab-${tabName}`)
+      activeTabContent.classList.remove('hidden')
+      activeTabContent.classList.add('active-tab')
+
+      // if (tabName === 'notices') {
+      //   viewAllButton.href = '/notices'
+      //   viewAllButton.textContent = 'সকল নোটিশ দেখুন'
+      // } else if (tabName === 'events') {
+      //   viewAllButton.href = '/events'
+      //   viewAllButton.textContent = 'সকল ইভেন্ট দেখুন'
+      // } else if (tabName === 'links') {
+      //   viewAllButton.href = '/quick-links'
+      //   viewAllButton.textContent = 'সকল লিংক দেখুন'
+      // }
+      // Re-add the arrow icon to the button
+      viewAllButton.innerHTML = `${viewAllButton.textContent} <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" /></svg>`
+    }
+
+    // Event listeners for tabs
+    tabs.forEach(tab => {
+      tab.addEventListener('click', () => {
+        activateTab(tab.dataset.tab)
+      })
+    })
+
+    // Initialize with the first tab active
+    activateTab('notices')
+  })
