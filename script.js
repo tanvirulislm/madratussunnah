25d2c04f41a750305fdf8b407dd719d1c5550467
Tanvirul Islam <ownbinodon@gmail.com>
2025-06-27 16:33:08 +0600

    f

--- script.js
+++ script.js
@@ -33,47 +33,35 @@ document.getElementById('mobileAcademicToggle').addEventListener('click', functi
   feather.replace()
 
 
-  
 
-  document.addEventListener('DOMContentLoaded', () => {
-    const tabs = document.querySelectorAll('.tab-button')
-    const tabContents = document.querySelectorAll('.tab-content')
-    const viewAllButton = document.getElementById('view-all-button')
-    const scrollContainer = document.querySelector('.custom-scrollbar')
-
-    function activateTab(tabName) {
-      // Deactivate all tab buttons
-      tabs.forEach(tab => {
-        tab.classList.remove('text-teal-600', 'border-teal-600')
-        tab.classList.add('text-gray-600', 'hover:text-teal-600', 'hover:border-teal-300')
+  // Slider For Branch
+  document.addEventListener('DOMContentLoaded', function () {
+        new Swiper('.myBranchSwiper', {
+          loop: true,
+          slidesPerView: 1,
+          spaceBetween: 24,
+          autoplay: {
+            delay: 1000,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: true,
+          },
+          breakpoints: {
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 24,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 32,
+            },
+            1280: {
+              slidesPerView: 3,
+              spaceBetween: 32,
+            },
+          },
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+        })
       })
-
-      tabContents.forEach(content => {
-        content.classList.remove('active-tab')
-        content.classList.add('hidden')
-      })
-      if (scrollContainer) {
-        scrollContainer.scrollTop = 0
-      }
-
-      const activeTabButton = document.querySelector(`[data-tab="${tabName}"]`)
-      activeTabButton.classList.remove('text-gray-600', 'hover:text-teal-600', 'hover:border-teal-300')
-      activeTabButton.classList.add('text-teal-600', 'border-teal-600')
-
-      const activeTabContent = document.getElementById(`tab-${tabName}`)
-      activeTabContent.classList.remove('hidden')
-      activeTabContent.classList.add('active-tab')
-
-      viewAllButton.innerHTML = `${viewAllButton.textContent} <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" /></svg>`
-    }
-
-    // Event listeners for tabs
-    tabs.forEach(tab => {
-      tab.addEventListener('click', () => {
-        activateTab(tab.dataset.tab)
-      })
-    })
-
-    // Initialize with the first tab active
-    activateTab('notices')
-  })
